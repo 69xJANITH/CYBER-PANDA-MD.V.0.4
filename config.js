b1362056815351343e589449d84dfac5513e3231
janith pramudya <121329858+69xJANITH@users.noreply.github.com>
2024-02-07 19:28:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
+global.Owner = ["+94765743034", "+94765743034"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94765743034", "+94765743034"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94765743034"];
+global.OwnerName = "ᴊᴀɴɪʏᴀ__ﾒ";
+global.BotName = "ᴊᴀɴɪʏᴀ__ﾒ";
 global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
 global.author = "KUSHAN";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
